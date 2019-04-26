4137b3c5fa8a8814d47d9aacc41fd177ff05685e
carmelogui <carmelogui@tencent.com>
2019-04-26 14:51:33 +0800

    add client send and recv test

--- Src/Client/SocketClient.cpp
+++ Src/Client/SocketClient.cpp
@@ -2,8 +2,13 @@
 
 int main(int argc, char * argv[])
 {
+	char SendBuf[] = { "Client connect\r\n" };
+	char RecvBuf[1024] = { 0 };
 	XTcp tcpClient;
 	tcpClient.Socket_Create();
 	tcpClient.Socket_Connect("10.0.2.15",8080);
+	tcpClient.Socket_Send(SendBuf,sizeof(SendBuf));
+	tcpClient.Socket_Recv(RecvBuf,sizeof(RecvBuf));
+	printf("RecvSever:%s\r\n", RecvBuf);
 	getchar();
 }
